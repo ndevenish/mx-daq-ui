fb4f1586b0ba8492681091a81571804f6756ec6e
Nicholas Devenish <ndevenish@gmail.com>
2026-07-31 11:44:41 +0100

    Update hardcoded visit

--- src/context/VisitProvider.tsx
+++ src/context/VisitProvider.tsx
@@ -1,7 +1,7 @@
 import { ReactNode, useState } from "react";
 import { VisitContext } from "./VisitContext";
 
-const defaultVisit = "cm44177-1"; // Until we can grab it after authentication
+const defaultVisit = "cm44177-3"; // Until we can grab it after authentication
 
 export const VisitProvider = ({ children }: { children: ReactNode }) => {
   const [visit, setVisit] = useState<string>(defaultVisit);
